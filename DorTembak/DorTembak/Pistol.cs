64f139cd1faccdc8ee22d4d1ed7488c5385ee238
beni1106 <revannovendriii3@gmail.com>
2025-07-02 16:03:31 +0700

    Add files via upload

---
+++ DorTembak/DorTembak/Pistol.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DorTembak
+{
+    public class Peluru
+    {
+        private int peluru;
+
+        public Peluru()
+        {
+
+        }
+        public void isiPeluru(int isi = 3)
+        {
+            peluru += isi;
+            Console.WriteLine($"Peluru diisi sebanyak {isi}. Total peluru : {peluru}");
+        }
+        public void tembak()
+        {
+            Console.WriteLine("Dor..");
+
+            if (peluru > 0)
+            {
+                peluru = peluru - 1;
+                Console.WriteLine($"Sisa peluru: {peluru}");
+            }
+            else
+            {
+                Console.WriteLine("Peluru habis");
+            }
+
+            if (peluru < 1)
+            {
+                Console.WriteLine("Peluru habis, segera isi ulang");
+            }
+        }
+
+        public void tampilkanPeluru()
+        {
+            Console.WriteLine($"Jumlah peluru saat ini: {peluru}");
+        }
+    }
+}
---
+++ DorTembak/DorTembak/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DorTembak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== CLASS PELURU ===\n");
+
+            Peluru senjata = new Peluru();
+
+            senjata.isiPeluru();
+
+            Console.WriteLine("\nMelakukan beberapa tembakan:");
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"\nTembakan ke-{i}:");
+                senjata.tembak();
+            }
+
+            Console.WriteLine("\nStatus berakhir:");
+            senjata.tampilkanPeluru();
+
+            Console.WriteLine("\nProgram selesai. ");
+            Console.ReadLine();
+        }
+    }
+}
